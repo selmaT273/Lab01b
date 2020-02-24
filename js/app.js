8638d6b1b1dc38f68c7c53039eb4601ab23f8fb9
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-24 15:53:58 -0600

    added console logs and styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function engagement(){
     var name = prompt('What is your name?');
+    console.log('User\'s name is ' + name);
 
     var residency = prompt('Are you an Iowa resident?');
     if (residency == 'no'){
@@ -7,16 +8,18 @@ function engagement(){
 
     } else if (residency == 'yes'){
         affiliation = prompt('Which party are you affiliated with? Republican or Democrat?');
+        console.log('User is an Iowa resident');
         participation = prompt('Did you participate in the 2020 Iowa caucus? yes or no');
 
         if (participation == 'yes'){
             document.write('Thank you ' + name + ' for participating in the political process. As an engaged ' + affiliation + ', you are a most honorable US citizen.');
+            console.log(name + ' participated in the 2020 Iowa caucus.');
 
         } else if (participation == 'no'){
             reason = prompt('why?');
             document.write('You ' + name + ' did not caucus because ' + reason + '. The entire ' + affiliation + ' party is deeply disappointed in you.');
+            console.log(name + ' did not participate in the 2020 Iowa caucus.');
         }
-
         
     } 
 
